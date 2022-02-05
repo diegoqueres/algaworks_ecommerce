70673cfe8bb0476c2b465bf0fdf5ec913f6fca58
Diego Queres <diegoqueres81@gmail.com>
2022-02-05 10:51:58 -0300

    3.4. Entendendo a diferença entre mapear atributos ou métodos

--- src/main/java/com/algaworks/ecommerce/model/Categoria.java
+++ src/main/java/com/algaworks/ecommerce/model/Categoria.java
@@ -1,26 +1,49 @@
 package com.algaworks.ecommerce.model;
 
 import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+/**
+ * As classes de entidade podem ser mapeadas também nos métodos get.
+ * Dessa forma, o mapeamento não fica mais localizado nos atributos.
+ */
 @Entity
 @Table(name = "categoria")
-@Getter
-@Setter
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Categoria {
 
-    @Id
     @EqualsAndHashCode.Include
     private Integer id;
     private String nome;
+    private Integer categoriaPaiId;
+
+    @Id
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
     @Column(name = "categoria_pai_id")
-    private Integer categoriaPaiId;
+    public Integer getCategoriaPaiId() {
+        return categoriaPaiId;
+    }
+
+    public void setCategoriaPaiId(Integer categoriaPaiId) {
+        this.categoriaPaiId = categoriaPaiId;
+    }
 }
