bcdb0933dff5719d8ab6bb25f571936b33d8cc73
Diego Queres <diegoqueres81@gmail.com>
2022-02-13 11:38:55 -0300

    4.3. Exercício: mapeando relacionamentos muitos-para-um

--- src/main/java/com/algaworks/ecommerce/model/Pedido.java
+++ src/main/java/com/algaworks/ecommerce/model/Pedido.java
@@ -7,6 +7,8 @@ import lombok.Setter;
 import javax.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "pedido")
