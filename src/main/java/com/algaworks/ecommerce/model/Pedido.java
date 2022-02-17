344365a682b63cfe65887c6f0a32dbe6041dddb0
Diego Queres <diegoqueres81@gmail.com>
2022-02-17 20:35:22 -0300

    4.13. Para o que serve o atributo optional?

--- src/main/java/com/algaworks/ecommerce/model/Pedido.java
+++ src/main/java/com/algaworks/ecommerce/model/Pedido.java
@@ -22,7 +22,7 @@ public class Pedido {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @ManyToOne
+    @ManyToOne(optional = false)
     @JoinColumn(name = "cliente_id")
     private Cliente cliente;
 
