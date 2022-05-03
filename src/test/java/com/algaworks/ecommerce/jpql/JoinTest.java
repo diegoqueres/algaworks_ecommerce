ec58844f01d08388350af636d32dd60da54572aa
Diego Queres <diegoqueres81@gmail.com>
2022-05-03 20:09:52 -0300

    9.8. Fazendo o join e usando o fetch

--- src/test/java/com/algaworks/ecommerce/jpql/JoinTest.java
+++ src/test/java/com/algaworks/ecommerce/jpql/JoinTest.java
@@ -14,6 +14,20 @@ import java.util.List;
 public class JoinTest extends EntityManagerTest {
 
     @Test
+    public void usarJoinFetch() {
+        //traz de uma vez só os dados da entidade e dos atributos lazy no join
+        String jpql = "select p from Pedido p "
+                + " left join fetch p.pagamento "
+                + " join fetch p.cliente "
+                + " left join fetch p.notaFiscal ";
+
+        TypedQuery<Pedido> typedQuery = entityManager.createQuery(jpql, Pedido.class);
+
+        List<Pedido> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+    }
+
+    @Test
     public void fazerLeftJoin() {
         String jpql = "select p from Pedido p left join p.pagamento pag on pag.status = 'PROCESSANDO'";
 
