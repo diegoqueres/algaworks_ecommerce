f97f1a62f84ab2301cd67db1f74dd0d2c2091752
Diego Queres <diegoqueres81@gmail.com>
2022-06-02 18:58:37 -0300

    9.23. Usando funções para datas

--- src/test/java/com/algaworks/ecommerce/jpql/FuncoesTest.java
+++ src/test/java/com/algaworks/ecommerce/jpql/FuncoesTest.java
@@ -1,17 +1,35 @@
 package com.algaworks.ecommerce.jpql;
 
 import com.algaworks.ecommerce.EntityManagerTest;
-import com.algaworks.ecommerce.model.Categoria;
 import org.junit.Assert;
 import org.junit.Test;
 
 import javax.persistence.TypedQuery;
 import java.util.List;
+import java.util.TimeZone;
 
-public class FuncoesStringsTest extends EntityManagerTest {
+public class FuncoesTest extends EntityManagerTest {
 
     @Test
-    public void aplicarFuncao() {
+    public void aplicarFuncaoData() {
+        // TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+        // current_date, current_time, current_timestamp
+        // year(p.dataCriacao), month(p.dataCriacao), day(p.dataCriacao)
+        // hour(p.dataCriacao), minute(p.dataCriacao), second(p.dataCriacao)
+
+        String jpql = "select hour(p.dataCriacao), minute(p.dataCriacao), second(p.dataCriacao) " +
+                "from Pedido p where hour(p.dataCriacao) >= 18";
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+
+        List<Object[]> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+
+        lista.forEach(arr -> System.out.println(arr[0] + " | " + arr[1] + " | " + arr[2]));
+    }
+
+    @Test
+    public void aplicarFuncaoString() {
         // Funções String: concat, length, locate, substring, lower, upper, trim
         //String jpql = "select c.nome, concat('Categoria: ', c.nome) from Categoria c";    //concat
         //String jpql = "select c.nome, length(c.nome) from Categoria c";     //length
