2869ae5ea78125cdfa8d3c3fe441c159474032ef
Diego Queres <diegoqueres81@gmail.com>
2022-06-09 20:10:06 -0300

    9.28. Agrupando o registros com group by

--- src/main/resources/META-INF/banco-de-dados/script-criacao.sql
+++ src/main/resources/META-INF/banco-de-dados/script-criacao.sql
@@ -1,3 +1,6 @@
+-- Rodar manualmente para SQL como root (GroupByTest.java)
+-- SET GLOBAL sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));
+
 create table testando (id integer not null auto_increment, primary key (id)) engine=InnoDB;
 
 create function acima_media_faturamento(valor double) returns boolean reads sql data return valor > (select avg(total) from pedido);
