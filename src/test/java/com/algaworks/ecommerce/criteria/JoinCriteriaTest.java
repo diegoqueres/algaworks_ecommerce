7e573a83441c3e762bf37183b5a908dd6293b075
Diego Queres <diegoqueres81@gmail.com>
2023-06-26 18:16:15 -0300

    10.9. Usando left outer join

--- src/test/java/com/algaworks/ecommerce/criteria/JoinCriteriaTest.java
+++ src/test/java/com/algaworks/ecommerce/criteria/JoinCriteriaTest.java
@@ -1,18 +1,32 @@
 package com.algaworks.ecommerce.criteria;
 
 import com.algaworks.ecommerce.EntityManagerTest;
-import com.algaworks.ecommerce.model.*;
+import com.algaworks.ecommerce.model.Pagamento;
+import com.algaworks.ecommerce.model.Pedido;
+import com.algaworks.ecommerce.model.StatusPagamento;
 import org.junit.Assert;
 import org.junit.Test;
 
 import javax.persistence.TypedQuery;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Join;
-import javax.persistence.criteria.Root;
+import javax.persistence.criteria.*;
 import java.util.List;
 
 public class JoinCriteriaTest extends EntityManagerTest {
+
+    @Test
+    public void fazerLeftOuterJoin() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Pedido> criteriaQuery = criteriaBuilder.createQuery(Pedido.class);
+        Root<Pedido> root = criteriaQuery.from(Pedido.class);
+        Join<Pedido, Pagamento> joinPagamento = root.join("pagamento", JoinType.LEFT);
+
+        criteriaQuery.select(root);
+
+        TypedQuery<Pedido> typedQuery = entityManager.createQuery(criteriaQuery);
+        List<Pedido> lista = typedQuery.getResultList();
+        Assert.assertTrue(lista.size() == 5);
+    }
+
     @Test
     public void fazerJoinComOn() {
         CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
