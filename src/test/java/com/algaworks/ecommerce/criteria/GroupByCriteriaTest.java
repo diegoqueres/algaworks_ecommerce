c08f79b39417a5711facad11ce16a651b8a175cf
Diego Queres <diegoqueres81@gmail.com>
2023-07-17 18:51:12 -0300

    10.30. Exercício: usando group by

--- src/test/java/com/algaworks/ecommerce/criteria/GroupByCriteriaTest.java
+++ src/test/java/com/algaworks/ecommerce/criteria/GroupByCriteriaTest.java
@@ -11,6 +11,32 @@ import java.util.List;
 public class GroupByCriteriaTest extends EntityManagerTest {
 
     @Test
+    public void agruparResultado03Exercicio() {
+//        Total de vendas por cliente
+//        String jpql = "select c.nome, sum(ip.precoProduto) from ItemPedido ip " +
+//                " join ip.pedido p join p.cliente c " +
+//                " group by c.id";
+
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Object[]> criteriaQuery = criteriaBuilder.createQuery(Object[].class);
+        Root<ItemPedido> root = criteriaQuery.from(ItemPedido.class);
+        Join<ItemPedido, Pedido> joinPedido = root.join(ItemPedido_.pedido);
+        Join<Pedido, Cliente> joinPedidoCliente = joinPedido.join(Pedido_.cliente);
+
+        criteriaQuery.multiselect(
+                joinPedidoCliente.get(Cliente_.nome),
+                criteriaBuilder.sum(root.get(ItemPedido_.precoProduto))
+        );
+
+        criteriaQuery.groupBy(joinPedidoCliente.get(Cliente_.id));
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(criteriaQuery);
+        List<Object[]> lista = typedQuery.getResultList();
+
+        lista.forEach(arr -> System.out.println("Nome cliente: " + arr[0] + ", Sum: " + arr[1]));
+    }
+
+    @Test
     public void agruparResultado02() {
 //        Total de vendas por categoria.
 //        String jpql = "select c.nome, sum(ip.precoProduto) from ItemPedido ip " +
